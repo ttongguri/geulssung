8e88884b87c81284981353e4abc895145fc4589f
minyeok <alsgurgud@naver.com>
2025-05-16 10:32:08 +0900

    migration, static, templates, base.html 생성

--- geulssung/core/models.py
+++ geulssung/core/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    nickname = models.CharField(max_length=30, unique=True)
+    nickname = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
         return self.nickname
