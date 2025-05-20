8b574cd3073c15ab60e538677aad5b268026b3f3
minyeok <alsgurgud@naver.com>
2025-05-20 10:08:42 +0900

    base templates에 test.html 작성(미리 css, 오류케이스 테스트)

--- geulssung/core/urls.py
+++ geulssung/core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home_view, name="home"),  # /geulssung/
+    path("test/", views.test_page_view, name="test"),  # /test/
 ]
--- geulssung/core/views.py
+++ geulssung/core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+# /test로 연결 되는 테스트 페이지 입니다.
+def test_page_view(request):
+    return render(request, "test.html")
+
+# /geulssung으로 연결 되는 메인 페이지 입니다.
 def home_view(request):
     return render(request, "base.html")
+
 
