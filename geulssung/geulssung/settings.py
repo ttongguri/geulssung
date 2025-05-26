f0e2639abdb40ab6badaa5d83a2ad093ba202681
minyeok <alsgurgud@naver.com>
2025-05-26 10:43:28 +0900

    게시글 커버 이미지 html 수정

--- geulssung/geulssung/settings.py
+++ geulssung/geulssung/settings.py
@@ -129,4 +129,9 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# 사용자 모델 설정
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# 미디어 파일 업로드 경로 설정
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
