de2c6ed66eff3d4667c253e99fcdf43c94207bf8
minyeok <alsgurgud@naver.com>
2025-05-27 10:16:00 +0900

    글감 구현 완료

--- geulssung/prompts/views.py
+++ geulssung/prompts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import GeneratedPrompt
+import random
 
-# Create your views here.
+def random_prompts(request):
+    style = request.GET.get('style')
+    genre = style  # style과 genre가 동일하게 쓰이는 구조
+    prompts = list(GeneratedPrompt.objects.filter(style=genre).values_list('content', flat=True))
+    selected = random.sample(prompts, min(4, len(prompts)))
+    return JsonResponse({'prompts': selected})
