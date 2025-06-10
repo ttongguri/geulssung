7c0fed8f0df9bd7d5c1c5127304b35fd32b1bf58
minyeok <alsgurgud@naver.com>
2025-06-10 15:50:17 +0900

    아 뭐 url 개판이야

--- geulssung/geulssung/urls.py
+++ geulssung/geulssung/urls.py
@@ -33,8 +33,8 @@ urlpatterns = [
     path("prompts/", include("prompts.urls")),
     path("geulssung/chat", chat_view, name="chat"),
     path("like/<int:post_id>/", views.like, name="like"),
-    path("geulssung/customizing/", include("customizing.urls")),
     path('my-items/', include('customizing.urls')),  # 너가 만든 앱의 URL 연결
+    path('geulssung/customizing/', include('customizing.urls')),
 ]
 
 if settings.DEBUG:
