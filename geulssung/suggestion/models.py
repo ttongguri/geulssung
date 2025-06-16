128489ecf415e978c032ab05a58ada8c79de4838
minyeok <alsgurgud@naver.com>
2025-06-16 13:37:55 +0900

    건의사항 페이지 개선

--- geulssung/suggestion/models.py
+++ geulssung/suggestion/models.py
@@ -7,6 +7,9 @@ class Suggestion(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    upvoted_users = models.ManyToManyField(CustomUser, related_name='upvoted_suggestions', blank=True)
+    downvoted_users = models.ManyToManyField(CustomUser, related_name='downvoted_suggestions', blank=True)
 
     def __str__(self):
         return f"(익명) {self.content[:20]}"
